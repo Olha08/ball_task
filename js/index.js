198102a7cfeffc2faa6b12fe84f92538d00fe16e
Olha <olha.bler@gmail.com>
2018-11-02 10:30:43 +0200

    initial

---
+++ js/index.js
@@ -0,0 +1,20 @@
+﻿function circle(step){
+    t = step + 0.05;
+
+    var r = -100;
+    var xcenter = 100;
+    var ycenter = 100;
+    var newLeft = xcenter + (r * Math.cos(t));
+    var newTop = ycenter + (r * Math.sin(t));
+
+    //console.log('left:'+newTop+' | top:'+newTop+' | t:'+t);
+
+    $('#ball').animate({
+        top: newTop,
+        left: newLeft,
+    }, 10, function() {
+        circle(t);
+    });
+}
+
+circle(0);
