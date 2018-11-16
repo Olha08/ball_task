f6a7a36293501bcdce4ffd8c56d06b181f708898
Olha <olha.bler@gmail.com>
2018-11-16 22:18:46 +0200

    added blink effect and change radius by click

--- js/index.js
+++ js/index.js
@@ -6,15 +6,25 @@
     var ycenter = 100;
     var newLeft = xcenter + (r * Math.cos(t));
     var newTop = ycenter + (r * Math.sin(t));
-
-    //console.log('left:'+newTop+' | top:'+newTop+' | t:'+t);
+    var newColor = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
 
     $('#ball').animate({
         top: newTop,
         left: newLeft,
+        backgroundColor: newColor
     }, 10, function() {
         circle(t);
     });
 }
 
 circle(0);
+
+function change_radius(){
+    var random_radius = Math.floor(Math.random() * 500) + 50;
+    random_radius = random_radius + 'px';
+
+    $('#ball').css('width', random_radius);
+    $('#ball').css('height', random_radius);
+    $('#ball').css('border-radius', random_radius);
+
+}
